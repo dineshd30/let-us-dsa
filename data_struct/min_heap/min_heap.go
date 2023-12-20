bed81dfa5166e0a1d37a6569baffbac0bc6ba627
Dinesh Lawate <100606811+dineshd30@users.noreply.github.com>
2023-12-20 09:24:39 +0000

    add heap/priority queue

---
+++ data_struct/min_heap/min_heap.go
@@ -0,0 +1,108 @@
+package main
+
+import "fmt"
+
+// Min heap data structure a.k.a. priority queue
+// With array index starting with 1
+// Get parent index of any node 			- index/2
+// Get left child index of any node 	- n*index
+// Get right child index of any node 	- n*index + 1
+type MinHeap struct {
+	Size  int
+	Array []int
+}
+
+// Create new min heap
+func NewMinHeap(len int) *MinHeap {
+	minHeap := &MinHeap{
+		Size:  0,
+		Array: make([]int, len),
+	}
+	minHeap.Array[0] = -1
+	return minHeap
+}
+
+// Insert into min heap
+func (m *MinHeap) Insert(v int) {
+	fmt.Printf("Inserting %d into min heap\n", v)
+
+	m.Size += 1
+	m.Array[m.Size] = v
+
+	smallestIndex := m.Size
+	for smallestIndex > 0 {
+		parentIndex := smallestIndex / 2                   // Get parent index of smallest
+		if m.Array[smallestIndex] < m.Array[parentIndex] { // Check if smallest is smaller than parent
+			m.SwapValue(parentIndex, smallestIndex) // Swap smallest with parent
+			smallestIndex = parentIndex             // Parent is new smallest
+			continue
+		}
+		break
+	}
+}
+
+// Swap values in min heap array
+func (m *MinHeap) SwapValue(i, j int) {
+	m.Array[i], m.Array[j] = m.Array[j], m.Array[i]
+}
+
+// Delete from min heap - It always deletes the root node
+func (m *MinHeap) Delete() {
+	fmt.Println("Deleting from the min heap")
+	m.Array[1] = m.Array[m.Size]
+	m.Array[m.Size] = -1
+	m.Size -= 1
+
+	for index := 1; index < m.Size; index++ {
+		smallestIndex := index
+		leftChildIndex := index * 2
+		rightChildIndex := index*2 + 1
+		fmt.Printf("leftChildIndex %d rightChildIndex %d\n", leftChildIndex, rightChildIndex)
+
+		if leftChildIndex <= m.Size && m.Array[leftChildIndex] < m.Array[smallestIndex] {
+			smallestIndex = leftChildIndex
+		}
+
+		if rightChildIndex <= m.Size && m.Array[rightChildIndex] < m.Array[smallestIndex] {
+			smallestIndex = rightChildIndex
+		}
+
+		fmt.Printf("index %d smallestIndex %d\n", index, smallestIndex)
+		if index != smallestIndex {
+			m.SwapValue(index, smallestIndex)
+		} else {
+			return
+		}
+	}
+}
+
+// Heapify min heap
+func (m *MinHeap) Heapify(index int) {
+	fmt.Printf("Heapifying min heap for index %d\n", index)
+}
+
+// Print min heap
+func (m *MinHeap) Print() {
+	for i := 1; i <= m.Size; i++ {
+		fmt.Printf("%d  ", m.Array[i])
+	}
+	fmt.Println()
+}
+
+func main() {
+	fmt.Println("Implementing min heap")
+	minHeap := NewMinHeap(10)
+	minHeap.Insert(22)
+	minHeap.Print()
+	minHeap.Insert(44)
+	minHeap.Print()
+	minHeap.Insert(12)
+	minHeap.Print()
+	minHeap.Insert(67)
+	minHeap.Print()
+	minHeap.Insert(24)
+	minHeap.Print()
+	minHeap.Delete()
+	minHeap.Print()
+	fmt.Printf("Min heap size - %d\n", minHeap.Size)
+}
