53cc145067a96c170bd43d45c5e4c4cb6b465a39
Dinesh Lawate <100606811+dineshd30@users.noreply.github.com>
2023-12-23 11:07:10 +0000

    add selection sort

---
+++ algo/sorting/selection_sort/selection_sort.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+// This function sorts given array in ascending order
+// We call it selection sort because in every pass the smallest value index is selected and inserted into its correct position
+func selectionSort(arr []int) []int {
+	lastIndex := len(arr) - 1
+
+	for i := 0; i <= lastIndex-1; i++ {
+		lowIndex := i
+		
+		for j := i + 1; j <= lastIndex; j++ {
+			if arr[j] < arr[lowIndex] {
+				lowIndex = j
+			}
+		}
+
+		if lowIndex != i {
+			arr[lowIndex], arr[i] = arr[i], arr[lowIndex]
+		}
+	}
+
+	return arr
+}
+
+func main() {
+	fmt.Println("Implementing selection sort")
+	arr := []int{33, 1, 44, 32, 7, 23}
+	fmt.Printf("Sorted array - %v\n", selectionSort(arr))
+}
