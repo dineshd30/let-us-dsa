d7ac458818a5770f0ddc7a4b8b613e6848c99ada
Dinesh Lawate <100606811+dineshd30@users.noreply.github.com>
2024-01-15 18:10:33 +0000

    add quick sort

---
+++ algo/sorting/qucik_srot/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func partition(arr []int) ([]int, int, []int) {
+	pivot := len(arr) - 1
+	i, j := 0, len(arr)-1
+	for i != j {
+		if arr[i] < arr[pivot] {
+			i += 1
+			continue
+		}
+
+		if arr[j] >= arr[pivot] {
+			j -= 1
+			continue
+		}
+
+		arr[i], arr[j] = arr[j], arr[i]
+	}
+
+	if i == j && pivot != j {
+		arr[pivot], arr[i] = arr[i], arr[pivot]
+	}
+
+	return arr[:i], arr[i], arr[i+1:]
+}
+
+func quickSort(arr []int) []int {
+	// base case
+	if len(arr) <= 1 {
+		return arr
+	}
+
+	left, middle, right := partition(arr)
+	sortedLeft := quickSort(left)
+	sortedRight := quickSort(right)
+	return append(append(sortedLeft, middle), sortedRight...)
+}
+
+func main() {
+	fmt.Println("Implementing quick sort")
+	arr := []int{10, 5, 11, 33, 7, 2, 1, 22, 1}
+	fmt.Printf("%d\n", arr)
+	fmt.Printf("%d\n", quickSort(arr))
+}
